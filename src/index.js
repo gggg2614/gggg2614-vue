f620038c14ff44dcce2a15afa1f5468bcf346396
lyi <839857519@qq.com>
2023-07-01 15:40:43 +0800

    vnode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { reactive } from "./reactive/reactive";
-import { effect } from "./reactive/effect";
-import { ref } from "./reactive/ref";
-import { computed } from "./reactive/computed";
+// import { reactive } from "./reactive/reactive";
+// import { effect } from "./reactive/effect";
+// import { ref } from "./reactive/ref";
+// import { computed } from "./reactive/computed";
 // const observed = (window.observed = reactive({
 //   count: 0,
 // }));
@@ -34,13 +34,39 @@ import { computed } from "./reactive/computed";
 //   console.log('foo: ', foo.value)
 // });
 
-const num = (window.num = ref(0));
-const c = (window.c = computed({
-  get() {
-    console.log('get');
-    return num.value * 2;
+// const num = (window.num = ref(0));
+// const c = (window.c = computed({
+//   get() {
+//     console.log('get');
+//     return num.value * 2;
+//   },
+//   set(val) {
+//     num.value = val;
+//   }, 
+// }));
+
+import { render, h, Text } from './runtime';
+
+const vnode = h(
+  'div',
+  {
+    class: 'a b',
+    style: {
+      border: '1px solid',
+      fontSize: '14px',
+    },
+    onClick: () => console.log('click'),
+    checked: '',
+    custom: false,
   },
-  set(val) {
-    num.value = val;
-  }, 
-}));
+  [
+    h('ul', null, [
+      h('li', { style: { color: 'red' } }, 1),
+      h('li', null, 2),
+      h('li', { style: { color: 'blue' } }, 3),
+      h('li', null, [h(Text, null, 'hello world')]),
+    ]),
+  ]
+);
+
+render(vnode, document.body);
---
+++ src/runtime/vnode.js
@@ -0,0 +1,45 @@
+import { isArray, isString, isNumber } from "../utils";
+
+export const ShapeFlags = {
+  ELEMENT: 1, // 00000001
+  TEXT: 1 << 1, // 00000010
+  FRAGMENT: 1 << 2, // 00000100
+  COMPONENT: 1 << 3, // 00001000
+  TEXT_CHILDREN: 1 << 4, // 00010000
+  ARRAY_CHILDREN: 1 << 5, // 00100000
+  CHILDREN: (1 << 4) | (1 << 5), //00110000
+};
+
+export const Text = Symbol("Text");
+export const Fragment = Symbol("Fragment");
+
+/*
+ * @param {string|Object|Text|Fragment} type
+ * @param {Object|null} props
+ * @param {string|Array|null}children
+ * retruns VNode
+ */
+export function h(type, props, children) {
+  let shapeFlag = 0;
+  if (isString(shapeFlag)) {
+    shapeFlag = ShapeFlags.ELEMENT;
+  } else if (type === Text) {
+    shapeFlag = ShapeFlags.TEXT;
+  } else if (type === Fragment) {
+    shapeFlag = ShapeFlags.FRAGMENT;
+  } else {
+    shapeFlag = ShapeFlags.COMPONENT;
+  }
+
+  if (isString(children) || isNumber(children)) {
+    shapeFlag |= ShapeFlags.TEXT_CHILDREN;
+    children = children.toString();
+  } else if (isArray(children)) {
+    shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
+  }
+  return {
+    type,
+    props,
+    children,
+  };
+}
